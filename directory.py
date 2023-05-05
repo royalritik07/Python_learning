b3bee9cf45b3c888cb24576bd66a3f1cc49bcdff
Hitesh <rbnritik07@gmail.com>
2023-05-05 22:37:59 +0530

    Nambi_Narayana

---
+++ directory.py
@@ -0,0 +1,16 @@
+Biography = {
+"Nambi_Narayana":"He was an aerospace scientist",
+"Awards":"He recieves Padma Bhushan award in on 26 jan 2019",
+"Vikas":"He developed a rocket engine named Vikas",
+"Dates":[26, 12],
+"Education":{"Engineering":"He complited B.Tech. in Mechanical Engineering"}
+}
+print(Biography["Nambi_Narayana"])
+print(Biography["Awards"])
+print(Biography["Vikas"])
+print(Biography["Dates"])
+print(Biography["Education"]["Engineering"])
+
+print(Biography.keys())
+print(Biography.values())
+print(Biography.items())
